126a86033fb2c86fefdece2e4e1112c380d73729
Diana <didi_rosa@mail.ru>
2023-11-01 12:29:45 +0600

    lab2#6

---
+++ lab2/MavProject/src/main/java/Payment.java
@@ -0,0 +1,18 @@
+public class Payment {
+    private String FIO;
+    private int day;
+    private int month;
+    private int year;
+    private int cach;
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
